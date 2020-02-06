da603b2ef70e12e258fbb504210f536d52140a8a
Joaquin <jolescano.o@gmail.com>
2020-02-06 18:23:09 -0300

    creada vista agricultura de precision

---
+++ resources/views/agriculturaDePrecision.blade.php
@@ -0,0 +1,295 @@
+@extends('layouts.navbarLayout')
+
+@section('content')
+<div class="container d-flex justify-content-between col-md-12 bg-intro pr-0">
+    <div class="row no-gutters">
+
+        <div class="col-md-6 d-md-none">
+            <img src="/maquetacion/p-agricultura-precision/bg-intro-ap.png" class="img-fluid float-right" alt="Responsive image">
+        </div>
+        <div class="col-md-6">
+            <div class="p-5" id="productosDrones">
+                <p class="text-white pl-5 pt-5">
+                <h6 class="text-success montserrat300">SERVICIOS</h6>
+                <h2 class="text-white montserrat700">Agricultura de Precision (AP)</h2>
+                <h4 class="text-white montserrat300">
+                    La Agricultura de Precisi&oacute;n (AP) es el conjunto de tecnolog&iacute;as que se aplican al trabajo en el campo como im&aacute;genes de sat&eacute;lites, sensores, im&aacute;genes obtenidas por drones, mapeos de profundidad de suelo o napa y datos geogr&aacute;ficos como los monitores de rendimiento, que re&uacute;nen la informaci&oacute;n necesaria para entender la variabilidad del suelo y los cultivos. Gracias a la AP los productores pueden tomar decisiones m&aacute;s eficientes y objetivas sobre las semillas que van a plantar o el
+                    fertilizante a aplicar, seleccionar de forma correcta el campo donde van a sembrar, e incluso son capaces de predecir el rendimiento de la cosecha, lo que hace mucho m&aacute;s preciso el trabajo
+                </h4>
+                </p>
+            </div>
+        </div>
+
+        <div class="col-md-6 d-none d-md-block">
+            <img src="/maquetacion/p-agricultura-precision/bg-intro-ap.png" class="img-fluid float-right" alt="Responsive image">
+        </div>
+    </div>
+</div>
+
+<div class="container col-md-12 bg-white pt-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <h4 class="barlow700 pt-4 pb-3">ESTUDIOS DE SUELOS A PARTIR DE IM&Aacute;GENES SATELITALES<br>(MAPAS DE HETEROGENEIDAD AMBIENTAL)</h4>
+            <h5 class="barlow300">
+                Las tecnolog&iacute;as de hoy permiten acceder a informaci&oacute;n &uacute;nica y precisa que nos da un detalle pormenorizado de las caracter&iacute;sticas agron&oacute;micas de un establecimiento agropecuario.
+                Con la utilizaci&oacute;n de im&aacute;genes satelitales logramos determinar el potencial productivo de un campo a partir de un estudio multitemporal, as&iacute; podemos identificar los diferentes ambientes que lo conforman su potencial y orientaci&oacute;n de producci&oacute;n, tambi&eacute;n podemos calcular la superficie que cubren cada uno con el fin de determinar su % en el total del campo.
+            </h5>
+        </div>
+
+        <div class="col-md-10 pt-3 pb-3">
+            <div class="row">
+                <div class="col-md-6 pb-4">
+                    <img src="/maquetacion/p-agricultura-precision/estudio-01.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-md-6 pb-4">
+                    <img src="/maquetacion/p-agricultura-precision/estudio-02.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-md-6 pb-4">
+                    <img src="/maquetacion/p-agricultura-precision/estudio-03.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-md-6 pb-4">
+                    <img src="/maquetacion/p-agricultura-precision/estudio-04.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center pb-5">
+        <a href="/consulta" class="btn btn-lg btn-success text-white montserrat600 rounded-0">CONSULTENOS</a>
+    </div>
+</div>
+
+<div class="container col-md-12 bg-nav pt-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10 pt-1 pb-5">
+            <div class="row">
+                <div class="col-md-10 pb-4">
+                    <h4 class="barlow700 pt-4 pb-3">ESTIMACION SATELITAL DE RINDE SMART SAMPLING</h4>
+                    <h5 class="barlow300">
+                        La t&eacute;cnica de estimaci&oacute;n de rinde por Smart Sampling fue desarrollada por G&D (premio INNOVAR 2010) para lograr brindar un servicio de calidad y precisi&oacute;n que nos permite estimar el rinde a cosecha de cualquier cultivo en cualquier etapa de su desarrollo, logrando estimar su rinde futuro. Esto permite poder realizar ventas anticipadas, logrando mejores precios o diagramar la log&iacute;stica de transporte y cosecha.
+                    </h5>
+                </div>
+                <div class="col-md-12 pt-2 rounded shadow">
+                    <img src="/maquetacion/p-agricultura-precision/clasificacion-pixel.jpg" class="img-fluid" alt="Responsive image">
+                    <h5 class="barlow600 pt-2">SMART SAMPLING AUTOGESTIONABLE</h5>
+                </div>
+            </div>
+            <div class="text-center pt-5">
+                <a href="/consulta" class="btn btn-lg btn-success text-white montserrat600 rounded-0">CONSULTENOS</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container col-md-12 bg-white pt-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <h4 class="barlow700 pt-4 pb-3">MAPEO DE PROFUNDIDAD DE SUELO</h4>
+            <h5 class="barlow300">
+                El conocer la profundidad efectiva de nuestros suelos nos permite poder tomar decisiones agron&oacute;micas con el fin de definir que cultivo realizar y que dosis de semilla y fertilizantes aplicar. En funci&oacute;n de la zona del pa&iacute;s en la que produzcamos el tipo de limitante de profundidad varia, en el sud este de la provincia de buenos aires y la pampa, la limitante es una plancha de tosca, en el norte puede ser piedra o antiguos cauces de r&iacute;os, en el sur puede ser piedra o calc&aacute;reo y en otras zonas existen horizontes thaptos o verticos, que se comportan como una limitante al crecimiento de las ra&iacute;ces sobre todo en &eacute;pocas secas.
+                G&D desarrollo un equipamiento hidr&aacute;ulico propio que permite ajustar y calibrar su utilizaci&oacute;n para cualquiera de estos escenarios y as&iacute; lograr un mapa de profundidad del perfil preciso, que luego puede ser usado para hacer aplicaci&oacute;n variable de insumos.
+            </h5>
+        </div>
+
+        <div class="col-md-10 pt-3 pb-5">
+            <div class="row">
+                <div class="col-md-6 pb-4">
+                    <img src="/maquetacion/p-agricultura-precision/mapeo-p-1.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-md-6">
+                    <img src="/maquetacion/p-agricultura-precision/mapeo-p-2.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-md-12">
+                    <img src="/maquetacion/p-agricultura-precision/mapeo-p-3.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center pt-2 pb-5">
+        <a href="/consulta" class="btn btn-lg btn-success text-white montserrat600 rounded-0">CONSULTENOS</a>
+    </div>
+</div>
+
+<div class="container col-md-12 bg-video pt-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10 pt-1 pb-5">
+            <div class="row">
+                <div class="col-md-10 pb-4">
+                    <h4 class="barlow700 pt-4 pb-3">MAPEO DE NAPAS CON GEORADAR</h4>
+                    <h5 class="barlow300">
+                        Conocer la profundidad a la que se encuentra la napa es muy &uacute;til, para la toma de decisiones. G&D a partir de su servicio de mapeo de napas con Georadar, le entrega al productor un mapa de la distribuci&oacute;n de la napa a lo largo de los lotes y as&iacute; poder ambientar el campo en zonas con efecto de napa y sin efecto de napa, para luego realizar una aplicaci&oacute;n variable de siembra, logrando de estar forma maximizar el potencial productivo de cada ambiente. </h5>
+                    </h5>
+                </div>
+                <div class="col-md-12 pt-2 rounded shadow">
+                    <video controls width="100%" height="85%" class="pt-3">
+                        <source src="/maquetacion/p-agricultura-precision/videos/Mapeo de Napas con Georadar II.mp4" type="video/mp4">
+                    </video>
+                    <h5 class="barlow600 pt-2">MAPEO DE NAPA</h5>
+                </div>
+                <div class="col-md-12 pt-4">
+                    <div class="row">
+                        <div class="col-md-6 pb-4">
+                            <img src="/maquetacion/p-agricultura-precision/mapeo-n-1.jpg" class="img-fluid" alt="Responsive image">
+                        </div>
+                        <div class="col-md-6 pb-4">
+                            <img src="/maquetacion/p-agricultura-precision/mapeo-n-2.jpg" class="img-fluid" alt="Responsive image">
+                        </div>
+                        <div class="col-md-6 pb-4">
+                            <img src="/maquetacion/p-agricultura-precision/mapeo-n-3.jpg" class="img-fluid" alt="Responsive image">
+                        </div>
+                        <div class="col-md-6 pb-4">
+                            <img src="/maquetacion/p-agricultura-precision/mapeo-n-4.jpg" class="img-fluid" alt="Responsive image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center pt-5">
+                <a href="/consulta" class="btn btn-lg btn-success text-white montserrat600 rounded-0">CONSULTENOS</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container col-md-12 bg-white pt-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <h4 class="barlow700 pt-4 pb-3">MUESTREO SATELITAL DE SUELOS</h4>
+            <h5 class="barlow300">
+                El muestreo satelital de suelos se basa en la utilizaci&oacute;n de im&aacute;genes satelitales con el fin de detectar los lugares donde se deben tomar las muestras de suelo. Gracias al an&aacute;lisis satelital podemos detectar la variabilidad de cada lote y as&iacute; muestrear el suelo de manera separada para cada ambiente. El proceso le da objetividad a la decisi&oacute;n de los lugares a muestrear, ya que el usuario va al campo con una grilla de puntos de donde debe tomar el dato, esto diferencia al muestreo tradicional en el cual el operario toma la muestra en base a su interpretaci&oacute;n visual del terreno. Nuestra metodolog&iacute;a adem&aacute;s de ser precisa, permite georeferenciar los sitios de muestreo con el fin de que campaña tras campañas las muestras se tomen en el mismo sitio y as&iacute; poder comparar la evoluci&oacute;n de cada nutriente a lo largo del tiempo de manera objetiva
+            </h5>
+        </div>
+
+        <div class="col-md-10 pt-3 pb-5">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="/maquetacion/p-agricultura-precision/muestreo.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center pt-2 pb-5">
+        <a href="/consulta" class="btn btn-lg btn-success text-white montserrat600 rounded-0">CONSULTENOS</a>
+    </div>
+</div>
+
+<div class="container col-md-12 bg-nav pt-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <h4 class="barlow700 pt-4 pb-3">PROCESAMIENTO DE MONITORES DE RINDE</h4>
+            <h5 class="barlow300">
+                Los datos de los monitores de rinde brindan excelente informaci&oacute;n sobre el comportamiento del cultivo y el efecto de las pr&aacute;cticas agron&oacute;micas ejecutadas durante el ciclo del mismo, o tambi&eacute;n nos permite evaluar el efecto del clima sobre el mismo como ser heladas o sequias. Para que esto sea representativo de la realidad, es necesario procesar y eliminar los datos err&oacute;neos que se registran durante la cosecha, G&D desarrollo un software que permite obtener la informaci&oacute;n correcta y filtrar los datos que no corresponden, lo que le permite al productor analizar los datos y tomar decisiones correctas gracias al servicio de procesamiento de Monitores de Rinde de G&D.
+            </h5>
+        </div>
+
+        <div class="col-md-10 pt-3 pb-5">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="/maquetacion/p-agricultura-precision/procesamiento.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center pt-2 pb-5">
+        <a href="/consulta" class="btn btn-lg btn-success text-white montserrat600 rounded-0">CONSULTENOS</a>
+    </div>
+</div>
+
+<div class="container col-md-12 bg-white pt-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <h4 class="barlow700 pt-4 pb-3">RASTRA VERIS</h4>
+            <h5 class="barlow300">Esta tecnolog&iacute;a permite relevar de manera r&aacute;pida, la CE del suelo, de esta manera se confeccionan mapas que permiten observar la variabilidad de cada lote.  </h5>
+        </div>
+
+        <div class="col-md-10 pt-3 pb-5">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="/maquetacion/p-agricultura-precision/rastraveris.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center pt-2 pb-5">
+        <a href="/consulta" class="btn btn-lg btn-success text-white montserrat600 rounded-0">CONSULTENOS</a>
+    </div>
+</div>
+
+<div class="container col-md-12 bg-nav pt-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <h4 class="barlow700 pt-4 pb-3">RELEVAMIENTO GPS</h4>
+         </div>
+
+        <div class="col-md-10 pt-3 pb-5">
+            <div class="row">
+                <div class="col-md-6 pb-4">
+                    <img src="/maquetacion/p-agricultura-precision/relevamiento-1.jpg" class="img-fluid" alt="Responsive image">
+                    <h4 class="barlow700 pt-4 pb-3">ALTIMETRIA</h4>
+                    <h5 class="barlow300">Gracias a nuestra experiencia y calidad de servicios, G&D obtiene la topograf&iacute;a de los lotes utilizando diferente herramienta tecnol&oacute;gicas, como ser, Drones, GPS geod&eacute;sicos y Lidar. Esto permitir&aacute; contar con informaci&oacute;n para poder realizar un correcto estudio del movimiento del agua dentro de una zona
+                    </h5>
+                </div>
+                <div class="col-md-6">
+                    <img src="/maquetacion/p-agricultura-precision/relevamiento-2.jpg" class="img-fluid" alt="Responsive image">
+                    <h4 class="barlow700 pt-4 pb-3">PLANIMETRIA</h4>
+                    <h5 class="barlow300">Las medici&oacute;n de precisi&oacute;n que realiza G&D utilizando la &uacute;ltima tecnolog&iacute;a, permite poder armar la estructura inicial para comenzar con un planteo de agricultura de precisi&oacute;n o manejo por ambientes. Adem&aacute;s la informaci&oacute;n generada le permite a la empresa, controlar las hect&aacute;reas facturadas por los contratistas, y as&iacute; pagar por lo que realmente se hace, estimar cantidad de insumos necesarios para cada lote o programar la log&iacute;stica de cosecha entre otras cosas.
+                    </h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center pt-2 pb-5">
+        <a href="/consulta" class="btn btn-lg btn-success text-white montserrat600 rounded-0">CONSULTENOS</a>
+    </div>
+</div>
+
+<div class="container col-md-12 bg-white pt-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <h4 class="barlow700 pt-4 pb-3">PRESCRIPCION VARIABLE DE INSUMOS</h4>
+            <h5 class="barlow300">Gracias a los avances tecnol&oacute;gicos ahora las sembradoras permiten poder realizar aplicaciones variables de insumos a partir de la ambientaci&oacute;n de los lotes utilizando varias fuentes de informaci&oacute;n georeferenciada como ser, mapas de rinde, im&aacute;genes satelitales de NDVI, mapeo de profundidad de suelos o mapeo de napas o altimetr&iacute;a, se puede analizar en conjunto todas estas capas y como una variable afecta a la otra, se genera el mapa de ambientes. Una vez obtenido este, el paso siguiente es agregar la dosis de fertilizante y semilla que queremos colocar en cada uno de los ambientes identificados con el fin de maximizar el potencial productivo de cada uno de estos. Esto mismo se puede realizar para la aplicaci&oacute;n variable de otros agroqu&iacute;micos para el control variable de malezas o plagas.
+                La prescripci&oacute;n variable de insumos es la posibilidad de variar la dosis de un producto a lo largo de la aplicaci&oacute;n utilizando como fuente de informaci&oacute;n un mapa de ambientes el cual se carga en la computadora del equipo junto con las dosis a colocar en cada ambiente.
+            </h5>
+        </div>
+
+        <div class="col-md-10 pt-3 pb-5">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="/maquetacion/p-agricultura-precision/prescripcion.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center pt-2 pb-5">
+        <a href="/consulta" class="btn btn-lg btn-success text-white montserrat600 rounded-0">CONSULTENOS</a>
+    </div>
+</div>
+
+<div class="container col-md-12 bg-nav pt-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <h4 class="barlow700 pt-4 pb-3">ESTIMACION DE &Aacute;REA SEMBRADA</h4>
+            <h5 class="barlow300">Utilizando t&eacute;cnicas de clasificaci&oacute;n supervisada, podemos estimar la superficie cubierta por un cultivo determinado a partir de su firma espectral, y as&iacute; conocer la superficie a nivel de un establecimiento rural en particular o a nivel de partido/departamento, provincia o a nivel Nacional, de cualquier pa&iacute;s y de cualquier cultivo.
+            </h5>
+        </div>
+
+        <div class="col-md-10 pt-3 pb-5">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="/maquetacion/p-agricultura-precision/estimacion.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center pt-2 pb-5">
+        <a href="/consulta" class="btn btn-lg btn-success text-white montserrat600 rounded-0">CONSULTENOS</a>
+    </div>
+</div>
+
+@endsection
