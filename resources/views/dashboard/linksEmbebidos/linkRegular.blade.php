66c4fb37d4d92cebb04525481d1170138bd1f1e2
fedeberon <federicoberon@outlook.com>
2020-10-19 16:00:07 -0300

    Fede * Se actualizan scrips de links embebidos de agrotoolbox

--- resources/views/dashboard/linksEmbebidos/linkRegular.blade.php
+++ resources/views/dashboard/linksEmbebidos/linkRegular.blade.php
@@ -14,24 +14,25 @@
 </head>
 <body style="height: 85%;">
 <div class="h-100">
+    <!--Inicio de CÃģdigo ATB REGULAR B2B v1.1-->
     <script type="text/javascript">// <![CDATA[
         window.__atb_b2b_root = {};
         (function () {
             var currentScript = document.currentScript || (function() {
-            var scripts = document.getElementsByTagName('script');
-            return scripts[scripts.length - 1];
+                var scripts = document.getElementsByTagName('script');
+                return scripts[scripts.length - 1];
             })();
             var atb = document.createElement('script');
             atb.type = 'text/javascript';
             atb.async = true;
             atb.time=new Date();
-            atb.src = 'http://index.agrotoolbox.com/b2b/wf.js';
+            atb.src = '//index.agrotoolbox.com/b2b/wff09524571GYD.js';
             var s = currentScript;
             s.parentNode.insertBefore(atb, s);
             window.__atb_b2b_root.node=s.parentNode;
         })();
         // ]]></script>
-
+    <!--Fin CÃģdigo ATB REGULAR B2B v1.1-->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
