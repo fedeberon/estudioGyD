9d547f94dfae59e5df735c89312ec6cc7c98b414
fedeberon <federicoberon@outlook.com>
2020-10-21 11:37:31 -0300

    Fede * Se agregan videos en seccion tuturiales.

--- resources/views/dashboard/tutoriales.blade.php
+++ resources/views/dashboard/tutoriales.blade.php
@@ -20,6 +20,60 @@
                         <a href="https://youtu.be/X2xiXA3ynpQ" class="btn btn-danger col-2 offset-2 mt-1"><i style="font-size: 34px;" class="fab fa-youtube"></i></a>
                         <a href="https://www.youtube.com/watch?v=84vqmWplZ6k" class="btn btn-danger col-2 offset-4 mt-1"><i style="font-size: 34px;" class="fab fa-youtube"></i></a>
                     </div>
+
+
+                    <hr/>
+                    <div class="row">
+                        <div class="alert alert-info col-6">
+                            Como pasar tu cuenta a premiun
+                        </div>
+
+                        <div class="alert alert-info col-6">
+                            Como registrarte a la Plataforma
+                        </div>
+
+                        <iframe class="col-6"  height="315" src="https://www.youtube.com/embed/bPhx3gKJd4g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                        <iframe class="col-6" height="315" src="https://www.youtube.com/embed/WuJG0ibFtRE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+
+
+                    <hr/>
+                    <div class="row">
+                        <div class="alert alert-info col-6">
+                            Como Descargar las im&aacute;genes de archivos
+                        </div>
+
+                        <div class="alert alert-info col-6">
+                            Como se interpretan las im&aacute;genes
+                        </div>
+
+                        <iframe class="col-6"  height="315" src="https://www.youtube.com/embed/pKLQ15C_TEI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                        <iframe class="col-6" height="315" src="https://www.youtube.com/embed/tavJbpG7vsQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+
+
+
+                    <hr/>
+                    <div class="row">
+                        <div class="alert alert-info col-6">
+                            Aprendiendo a utilizar la plataforma
+                        </div>
+
+                        <div class="alert alert-info col-6">
+                            Características de los Satelites utilizados
+                        </div>
+
+                        <iframe class="col-6"  height="315" src="https://www.youtube.com/embed/9uJCkJhMk1c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                        <iframe class="col-6" height="315" src="https://www.youtube.com/embed/cu6Mqe_Jr6U" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+
+
+
+
+
                 </div>
             </div>
         </div>
